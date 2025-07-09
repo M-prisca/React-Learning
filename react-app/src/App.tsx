6db74fccf0632d6cb591abb688a600977aa511f4
Prisca Masereli <priscamasereli@gmail.com>
2025-07-09 15:54:05 +0200

    doc: Add needed comments to App.tsx

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,17 @@
-
 import ListGroup from "./components/ListGroup";
 
-function App(){
-  return <div><ListGroup /> </div>
+function App() {
+  return (
+    <div>
+      <ListGroup />{" "}
+    </div>
+  );
 }
 export default App;
+// This is the main entry point of the React application.
+// It imports the ListGroup component and renders it within a div.
+// The ListGroup component is expected to handle the display of a list, likely with some interactive
+// functionality such as adding or removing items, or displaying a message when the list is empty.
+// The App component serves as the root component of the application, which can be expanded with more
+// components and features as needed.
+// The application is structured to allow for easy expansion and modification, with the ListGroup
