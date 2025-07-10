9a6a14dc471f005b378488b18b156bafe5a6f699
Prisca Masereli <priscamasereli@gmail.com>
2025-07-10 03:26:50 +0200

    feat(component listGroup): Managing state

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = [
@@ -8,10 +8,7 @@ function ListGroup() {
     "A fourth item",
     "And a fifth one",
   ];
-  // Event handler for click events
-  const handleClick = (event: MouseEvent) => {
-    console.log(event);
-  };
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
@@ -19,7 +16,17 @@ function ListGroup() {
       {items.length === 0 && <p> No item found!</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
