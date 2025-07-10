d8eaa7b65569f4b486720ba9201170b325761a7b
Prisca Masereli <priscamasereli@gmail.com>
2025-07-10 04:46:34 +0200

    feat(Button component): Exercise- build a button component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,7 +3,9 @@ import Button from "./components/Button";
 function App() {
   return (
     <div>
-      <Button>Click Me</Button>
+      <Button color="info" onClick={() => console.log("Button clicked!")}>
+        Click Me
+      </Button>
     </div>
   );
 }
