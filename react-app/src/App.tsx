66c4aab8388b7635a88509e05a0d6742223358dd
Prisca Masereli <priscamasereli@gmail.com>
2025-07-10 13:52:21 +0200

    feat(ListGroup): Styling components with vanilla css

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,19 +1,29 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+// import { useState } from "react";
+// import Alert from "./components/Alert";
+// import Button from "./components/Button";
+
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  const [alertVisible, setAlertVisible] = useState(false);
+  // const [alertVisible, setAlertVisible] = useState(false);
 
   return (
-    <div>
-      {alertVisible && (
+    <>
+      <ListGroup
+        items={["New York", "San Francisco", "Los Angeles"]}
+        heading="Cities"
+        onSelectItem={(item) => console.log(item)}
+      />
+    </>
+  );
+}
+<div>
+  {/* {alertVisible && (
         <Alert onClose={() => setAlertVisible(false)}> My Alert Message</Alert>
       )}
       <Button color="info" onClick={() => setAlertVisible(true)}>
         Click Me
-      </Button>
-    </div>
-  );
-}
+      </Button> */}
+</div>;
+//
 export default App;
