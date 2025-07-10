b2616e248c1f6dad8b867c7ecadb0c4c7ddc054c
Prisca Masereli <priscamasereli@gmail.com>
2025-07-10 04:04:06 +0200

    feat: Create Alert component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,32 +1,10 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = [
-    "An item",
-    "A second item",
-    "A third item",
-    "A fourth item",
-    "And a fifth one",
-  ];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="My List"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert />
     </div>
   );
 }
 export default App;
-// This is the main entry point of the React application.
-// It imports the ListGroup component and renders it within a div.
-// The ListGroup component is expected to handle the display of a list, likely with some interactive
-// functionality such as adding or removing items, or displaying a message when the list is empty.
-// The App component serves as the root component of the application, which can be expanded with more
-// components and features as needed.
-// The application is structured to allow for easy expansion and modification, with the ListGroup
