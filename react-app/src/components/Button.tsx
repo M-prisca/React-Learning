c9bf340961c4894a37ce32902b0173694861489d
Prisca Masereli <priscamasereli@gmail.com>
2025-07-10 04:34:32 +0200

    feat(Button component): React best practice Refactoring the component

--- react-app/src/components/Button.tsx
+++ react-app/src/components/Button.tsx
@@ -1,36 +1,9 @@
-const Button = () => {
-  return (
-    <div>
-      <button type="button" className="btn btn-primary">
-        Primary
-      </button>
-      <button type="button" className="btn btn-secondary">
-        Secondary
-      </button>
-      <button type="button" className="btn btn-success">
-        Success
-      </button>
-      <button type="button" className="btn btn-danger">
-        Danger
-      </button>
-      <button type="button" className="btn btn-warning">
-        Warning
-      </button>
-      <button type="button" className="btn btn-info">
-        Info
-      </button>
-      <button type="button" className="btn btn-light">
-        Light
-      </button>
-      <button type="button" className="btn btn-dark">
-        Dark
-      </button>
+interface Props {
+  children: string;
+}
 
-      <button type="button" className="btn btn-link">
-        Link
-      </button>
-    </div>
-  );
+const Button = ({ children }: Props) => {
+  return <button className="btn btn-primary">{children}</button>;
 };
 
 export default Button;
